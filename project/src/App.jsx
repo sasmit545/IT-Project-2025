a0f564fb5261bc13033b58e8a8fed1583f3d4122
sasmit <sasmitshashwat@gmail.com>
2025-02-06 14:08:55 +0530

    added config for tailwind css and intiated react app

---
+++ project/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='text-red-500' >IT-Project</h1>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2z39khUh_j7KWA6T7Y8ryQChn5dCVYxL0dg&s' className='w-full'  />
+        </div>      
+    </>
+  )
+}
+
+export default App
