4f5254c662e26301de4d4d03a901b6f31d790dce
shresth <shresthkasyap19@gmail.com>
2025-04-02 22:35:57 +0530

    Refactor user model export and add new website model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -63,5 +63,6 @@ userSchema.methods.generateRefreshToken = function() {
 
 
 
-export const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
+export default User;
---
+++ backend/src/models/website.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const WebsiteSchema = new mongoose.Schema({
+    sourcecode:{
+        type: String,
+        required: true,
+        default: ""
+    }
+    ,
+    owner:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    name:{
+        type: String,
+        required: true,
+        default: ""
+    }
+},{timestamps:true});
+
+const Website = mongoose.model("Website", WebsiteSchema);
+export default Website;
